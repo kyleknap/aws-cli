8c42c692329342f8240bedcbe6bc7a8cf798b57a
kyleknap <kyknapp1@gmail.com>
2020-01-15 13:50:22 -0800

    Fix setuptools hidden import issue

--- exe/pyinstaller/hook-awscli.py
+++ exe/pyinstaller/hook-awscli.py
@@ -2,6 +2,6 @@ from PyInstaller.utils.hooks import collect_data_files
 
 hiddenimports = ['docutils', 'urllib', 'httplib', 'html.parser',
                  'configparser', 'xml.etree', 'pipes', 'colorama',
-                 'awscli.handlers']
+                 'awscli.handlers', 'pkg_resources.py2_warn']
 datas = collect_data_files('awscli')
 
